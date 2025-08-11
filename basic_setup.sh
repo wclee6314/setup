2a9e8c4395a64e4dff356354333a340faa5cd264
wclee6314 <wclee@dnotitia.com>
2025-08-11 04:02:23 +0000

    []

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+# tmux
 apt install tmux
+
+# miniconda
+ARCH=$(uname -m); \
+if [ "$ARCH" = "x86_64" ]; then F=Miniconda3-latest-Linux-x86_64.sh; \
+elif [ "$ARCH" = "aarch64" ]; then F=Miniconda3-latest-Linux-aarch64.sh; \
+else echo "Unsupported arch: $ARCH"; exit 1; fi; \
+wget https://repo.anaconda.com/miniconda/$F && \
+bash $F -b -p $HOME/miniconda3 && \
+$HOME/miniconda3/bin/conda init "$(basename "$SHELL")" && \
+exec $SHELL -l
